3f965d3b892bf08ed922d7a9980ca47cded22673
Joram van den Boezem <joram@vandenboezem.nl>
2020-08-21 11:30:36 +0200

    setup test workflow

---
+++ test.js
@@ -0,0 +1,16 @@
+const which = require("which");
+
+const command = process.argv[2];
+
+console.log(`which("${command}")`);
+
+async function test() {
+  try {
+    const path = await which(command);
+    console.log(`"${command}" found in "${path}"`);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+test();
